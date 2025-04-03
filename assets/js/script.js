c78ad7e1969ca72560972edae47a3f66dd1d1d5a
ksstrat <qpvgrwku@students.codeinstitute.net>
2025-04-03 12:26:10 +0200

    feat: Add first content to script.js, declairing const variables

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//Wait for the DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function() {
+
+/**
+ * Declare constants for DOM elements and possible choices
+ */
+    const playerGesture = document.getElementById("playerGesture");
+    const compGesture = document.getElementById("compGesture");
+    const playerScore = document.getElementById("playerScore");
+    const compScore = document.getElementById("compScore");
+    const choicesBtns = document.getElementsByClassName("choicesBtn")
+    const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+    
+})
